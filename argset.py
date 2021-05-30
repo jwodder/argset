1618abeecca38fc2ad35655f84834601c34fd8e6
John T. Wodder II <git@varonathe.org>
2021-05-30 19:34:31 -0400

    Packaging

--- argset.py
+++
@@ -1,8 +0,0 @@
-from typing import FrozenSet, NamedTuple
-
-class ArgSet(NamedTuple):
-    required_positional_only: int
-    optional_positional_only: int
-    required_args: FrozenSet[str]
-    optional_args: FrozenSet[str]
-    takes_kwargs: bool
---
+++ src/argset/__init__.py
@@ -0,0 +1,20 @@
+"""
+Simple callable argument inspection & filtering
+
+Visit <https://github.com/jwodder/argset> for more information.
+"""
+
+__version__      = '0.1.0.dev1'
+__author__       = 'John Thorvald Wodder II'
+__author_email__ = 'argset@varonathe.org'
+__license__      = 'MIT'
+__url__          = 'https://github.com/jwodder/argset'
+
+from typing import FrozenSet, NamedTuple
+
+class ArgSet(NamedTuple):
+    required_positional_only: int
+    optional_positional_only: int
+    required_args: FrozenSet[str]
+    optional_args: FrozenSet[str]
+    takes_kwargs: bool
