68886abcefb065b33e78e0afbe72126fff3bd425
John T. Wodder II <git@varonathe.org>
2021-05-30 19:49:26 -0400

    Get tests to pass

--- src/argset/__init__.py
+++ src/argset/__init__.py
@@ -10,12 +10,27 @@ __author_email__ = "argset@varonathe.org"
 __license__ = "MIT"
 __url__ = "https://github.com/jwodder/argset"
 
-from typing import FrozenSet, NamedTuple
+from dataclasses import dataclass
+from typing import FrozenSet
 
 
-class ArgSet(NamedTuple):
+@dataclass
+class ArgSet:
     required_positional_only: int
     optional_positional_only: int
     required_args: FrozenSet[str]
     optional_args: FrozenSet[str]
     takes_kwargs: bool
+
+    @property
+    def positional_only(self) -> int:
+        return self.required_positional_only + self.optional_positional_only
+
+    @property
+    def argnames(self) -> FrozenSet[str]:
+        return self.required_args | self.optional_args
+
+    def __contains__(self, arg: str) -> bool:
+        return (
+            self.takes_kwargs or arg in self.required_args or arg in self.optional_args
+        )
